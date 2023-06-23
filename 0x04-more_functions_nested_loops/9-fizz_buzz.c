9a7f8614c14ba284d55448d21660fbb0277cd506
ismailzda <zdasmail7@gmail.com>
2023-06-22 17:53:42 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,15 +15,15 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
 {
-printf(" Fizz");
+printf("Fizz");
 }
 else if (i % 5 == 0 && i % 3 != 0)
 {
-printf(" Buzz");
+printf("Buzz");
 }		
 else if (i % 3 == 0 && i % 5 == 0)
 {
-printf(" FizzBuzz");
+printf("FizzBuzz");
 }
 else if (i == 1)
 {
@@ -31,10 +31,9 @@ printf("%d", i);
 }
 else
 {
-printf(" %d", i);
+printf("%d", i);
 }
 }
 printf("\n");
-
 return (0);
 }
