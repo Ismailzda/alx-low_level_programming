91b2d9bcd80ad854e1fdcb91ad25305efee3236e
ismailzda <zdasmail7@gmail.com>
2023-06-22 17:42:46 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,24 @@
 #include "main.h"
 
 /**
-*more_numbers - print more numbers
+*print_line - draws a straight line in the terminal
+*@n: number of times the character _ should be printed
 */
+void print_line(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i;
 
-void more_numbers(void)
+for (i = 1; i <= n; i++)
 {
-int i, j;
-
-for (i = 1; i <= 10; i++)
-{
-for (j = 0; j <= 14; j++)
-{
-if (j >= 10)
-_putchar('1');
-_putchar (j % 10 + '0');
+_putchar('_');
 }
 _putchar('\n');
 }
+
 }
