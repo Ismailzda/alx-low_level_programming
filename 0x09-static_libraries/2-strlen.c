98f62982caf5369981da21f43f811eedcd99f7fb
ismailzda <zdasmail7@gmail.com>
2023-07-10 10:32:03 -0700

    update

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,27 +1,18 @@
 #include "main.h"
 /**
-*_strncpy - copy a string
-*@dest: input value
-*@src: input value
-*@n: input value
-*
-*Return: dest
+*_strlen - returns the length of a string
+*@s: string
+*Return: length
 */
-char *_strncpy(char *dest, char *src, int n)
+int _strlen(char *s)
 {
-int j;
+int longi = 0;
 
-j = 0;
-while (j < n && src[j] != '\0')
+while (*s != '\0')
 {
-dest[j] = src[j];
-j++;
-}
-while (j < n)
-{
-dest[j] = '\0';
-j++;
+longi++;
+s++;
 }
 
-return (dest);
+return (longi);
 }
